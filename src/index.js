2c79fe52eedeb2bf63bd86c2b88342d43cfa315e
Stu Pender <stu1992@gmail.com>
2021-11-24 22:55:09 -0600

    full-website

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// NAV TOGGLE
+let iphoneMenuLink = document.querySelector('#iphone');
+let menuItems = document.querySelector('#iphone-menu');
+let main = document.querySelector('main');
+let about = document.querySelector('.about');
+let material = document.querySelector('.material');
+let pricing = document.querySelector('.pricing');
+let body = document.querySelector('body');
+
+
+document.querySelector('#iphone').addEventListener("click", navToggle);
+document.querySelector('.about').addEventListener("click", navToggle);
+document.querySelector('.material').addEventListener("click", navToggle);
+document.querySelector('.pricing').addEventListener("click", navToggle);
+
+
+function navToggle() {
+    menuItems.classList.toggle('active');
+    main.classList.toggle('hidden');
+    body.classList.toggle('menu-color');                          
+}
